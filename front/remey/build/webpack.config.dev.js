9ff9c2988f67cce02a418644b9b178635b452f5d
tuskmon <task320@gmail.com>
2018-10-02 23:50:47 +0900

    build dev

---
+++ front/remey/build/webpack.config.dev.js
@@ -0,0 +1,23 @@
+'use strict'
+const { VueLoaderPlugin } = require('vue-loader')
+module.exports = {
+  mode: 'development',
+  entry: [
+    './src/main.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+}
